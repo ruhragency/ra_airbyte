5a52b5b3d2ab63f9545938ad32c7a20a82f28cb3
Sebastian Schult <sebastian.schult@ruhr.agency>
2022-09-27 09:05:52 +0200

    add dependencies

--- airbyte-integrations/connectors/source-mollie/setup.py
+++ airbyte-integrations/connectors/source-mollie/setup.py
@@ -7,12 +7,14 @@ from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
     "airbyte-cdk~=0.1.56",
+    "mollie-api-python~=2.13.0",
 ]
 
 TEST_REQUIREMENTS = [
     "pytest~=6.1",
     "pytest-mock~=3.6.1",
     "source-acceptance-test",
+  
 ]
 
 setup(
